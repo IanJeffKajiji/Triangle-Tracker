0b034dad3e2957f7f5dd1961966667d9034c3911
Ian Jeff <iankajiji@gmail.com>
2019-02-17 13:55:55 +0300

    add change in js

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,21 @@ function track() {
   var sideA = parseFloat(prompt("Enter value of hypotenuse"));
   var sideB = parseFloat(prompt("Enter value of opposite side"));
   var sideC = parseFloat(prompt("Enter value of adjacent"));
-  var triangle = ["Equilateral", "isosceless", "scalene", "Not a triangle"];
+  var triangle = ["Equilateral triangle", "Isosceless triangle", "Scalene triangle", "Not a triangle"];
 
 if (sideA <= 0 || sideB <= 0 || sideC <=0) {
   alert("invalid character");
 }
-else{if (sideA === sideB && sideB === sideC) {
+else if (sideA === sideB && sideB === sideC) {
   alert (triangle[0]);
 }
 else if(sideA===sideB || sideB===sideC || sideA===sideC) {
   alert (triangle[1]);
 }
-else if(sideA!==sideB || sideB!==sideC || sideA!==sideC) {
+if (sideA!==sideB || sideB!==sideC || sideA!==sideC) {
   alert(triangle[2]);
 }
-else {
+else if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)) {
   alert(triangle[3]);
-}}
+}
 };
